0fb0d9f078adb354f6e00163a5e4f299a4042fe8
Michael Noronha <michaeltnoronha@gmail.com>
2021-12-06 03:27:26 -0600

    day 5

---
+++ day05/part1.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+counts = defaultdict(int)
+
+with open("input.txt") as f:
+    for line in f:
+        l, r = line.strip().split(" -> ")
+        x1, y1 = map(int, l.split(","))
+        x2, y2 = map(int, r.split(","))
+
+        if x1 != x2 and y1 != y2:
+            continue
+
+        if y1 == y2:
+            for x in range(min(x1,x2), max(x1,x2)+1):
+                counts[(x, y1)] += 1
+
+        if x1 == x2:
+            for y in range(min(y1,y2), max(y1,y2)+1):
+                counts[(x1, y)] += 1
+
+tot = 0
+for x,y in counts:
+    if counts[(x,y)] > 1:
+        tot += 1
+print(tot)
---
+++ day05/part2.py
@@ -0,0 +1,32 @@
+
+from collections import defaultdict
+
+counts = defaultdict(int)
+
+with open("input.txt") as f:
+    for line in f:
+        l, r = line.strip().split(" -> ")
+        x1, y1 = map(int, l.split(","))
+        x2, y2 = map(int, r.split(","))
+
+        if x1 != x2 and y1 != y2:
+            assert abs(x1-x2) == abs(y1-y2)
+            steps = abs(x1-x2) + 1
+            dx = -1 if x1 > x2 else 1
+            dy = -1 if y1 > y2 else 1
+            for step in range(steps):
+                counts[(x1+dx*step,y1+dy*step)] += 1
+
+        elif y1 == y2:
+            for x in range(min(x1,x2), max(x1,x2)+1):
+                counts[(x, y1)] += 1
+
+        elif x1 == x2:
+            for y in range(min(y1,y2), max(y1,y2)+1):
+                counts[(x1, y)] += 1
+
+tot = 0
+for x,y in counts:
+    if counts[(x,y)] > 1:
+        tot += 1
+print(tot)
